8b58aace8369e00a5b124330fd6db686d8893e4b
Marc Foley <m.foley.88@gmail.com>
2022-09-09 10:40:41 +0100

    ft_hb_shape: simplify

--- diffenator/ft_hb_shape.py
+++ diffenator/ft_hb_shape.py
@@ -1,37 +1,17 @@
-import freetype as ft
-import numpy as np
-import uharfbuzz as hb
 import logging
-import tempfile
 from blackrenderer.render import renderText
-from PIL import Image
 
 logger = logging.getLogger(__name__)
 
 
-def draw_text(
-    filename,
-    text,
-    size=64,
-    features=None,
-    script=None,
-    lang=None,
-):
-    with tempfile.NamedTemporaryFile(suffix=".png") as out:
-        renderText(filename, text, outputPath=out.name, fontSize=size, backendName="skia", script=str(script), lang=str(lang))
-        img = Image.open(out.name)
-        arr = np.asarray(img)
-    return arr
-
 
 if __name__ == "__main__":
     import argparse
-    import re
 
     parser = argparse.ArgumentParser(description="Draw some text")
     parser.add_argument("font", metavar="TTF")
     parser.add_argument("string", metavar="TEXT")
-    parser.add_argument("--out", metavar="PNG", default="out.png")
+    parser.add_argument("--out", "-o", metavar="PNG", default="out.png")
     parser.add_argument("--lang", metavar="LANGUAGE")
     parser.add_argument("--script", metavar="SCRIPT")
     parser.add_argument("--features", metavar="FEATURES")
@@ -47,10 +27,4 @@ if __name__ == "__main__":
             else:
                 features[f] = True
 
-    img = draw_text(
-        args.font, args.string, lang=args.lang, script=args.script, features=features
-    )
-    from PIL import Image
-
-    I = Image.fromarray(img[::-1, ::1])
-    I.save(args.out)
+    renderText(args.font, args.string, args.out, features=features, lang=args.lang, script=args.script)
