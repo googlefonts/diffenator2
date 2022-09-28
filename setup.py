ecd8520c7743b9a62ce015a461312aacc834fa8d
Marc Foley <m.foley.88@gmail.com>
2022-09-28 10:23:09 +0100

    move ninja_sketch to bin/diffenator2

--- setup.py
+++ setup.py
@@ -2,17 +2,27 @@ from setuptools import setup, find_packages
 from glob import glob
 
 setup(
-    name='Diffenator2',
-    version='0.0.1',
-    url='https://github.com/m4rc1e/diffenator2',
-    author='Marc Foley',
-    author_email='m.foley.88@gmail.com',
-    description='Compare two fonts.',
-    packages=find_packages(),    
-    install_requires=["FontTools[ufo]", "fontFeatures[shaper]", "jinja2", "blackrenderer[skia]", "Pillow", "uharfbuzz", "pyahocorasick", "selenium>=4.4.3"],
-    package_dir={'diffenator': 'diffenator'},
+    name="Diffenator2",
+    version="0.0.1",
+    url="https://github.com/m4rc1e/diffenator2",
+    author="Marc Foley",
+    author_email="m.foley.88@gmail.com",
+    description="Compare two fonts.",
+    packages=find_packages(),
+    install_requires=[
+        "FontTools[ufo]",
+        "fontFeatures[shaper]",
+        "jinja2",
+        "blackrenderer[skia]",
+        "Pillow",
+        "uharfbuzz",
+        "pyahocorasick",
+        "selenium>=4.4.3",
+        "ninja",
+    ],
+    package_dir={"diffenator": "diffenator"},
     package_data={
         "diffenator": ["data/*", "templates/*", "data/*.txt"],
     },
-    scripts=glob("bin/*")
+    scripts=glob("bin/*"),
 )
