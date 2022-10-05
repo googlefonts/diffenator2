074b32b7eb2ff28fa51e5bbc6fea92148688bb8c
Marc Foley <m.foley.88@gmail.com>
2022-10-05 12:22:47 +0100

    add diffenator test

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -50,15 +50,18 @@ def test_run_diffbrowsers_proof_imgs(fp):
 # TODO test diffbrowsers diff
 
 
-#@pytest.mark.parametrize(
-#    "fp_before, fp_after",
-#    [
-#        (mavenpro_vf, mavenpro_vf_mod)
-#    ]
-#)
-#def test_diffenator(fp_before, fp_after):
-#    with tempfile.TemporaryDirectory() as tmp_dir:
-#        subprocess.run(["diffenator", fp_before, fp_after, "-o", tmp_dir])
-#        assert os.path.basename(fp_before) in os.listdir(tmp_dir)
-#        assert os.path.basename(fp_after) in os.listdir(tmp_dir)
-#
+@pytest.mark.parametrize(
+    "fp_before, fp_after",
+    [
+        (mavenpro_vf, mavenpro_vf_mod),
+#        (mavenpro_vf, mavenpro_extra_bold),
+    ]
+)
+def test_diffenator(fp_before, fp_after):
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        subprocess.run(["diffenator", fp_before, fp_after, "-o", tmp_dir])
+        new_font = f"old-{os.path.basename(fp_before)}"
+        old_font = f"new-{os.path.basename(fp_before)}"
+        assert new_font in os.listdir(tmp_dir)
+        assert old_font in os.listdir(tmp_dir)
+        assert any(f.endswith(".html") for f in os.listdir(tmp_dir))
