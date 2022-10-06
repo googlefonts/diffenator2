1566667deb79642a79c37dcdcb3a347e09df38ef
Marc Foley <m.foley.88@gmail.com>
2022-10-06 11:15:10 +0100

    drop tempfile temporarily

--- diffenator/__init__.py
+++ diffenator/__init__.py
@@ -34,34 +34,30 @@ def run_proofing_tools(fonts, out="out", imgs=True):
     if not os.path.exists(out):
         os.mkdir(out)
 
-    with tempfile.TemporaryDirectory() as dir_:
-        w = Writer(open(os.path.join(dir_, "build.ninja"), "w"))
-        w.comment("Rules")
-        w.newline()
-        out_s = f"$out{os.path.sep}diffbrowsers"
+    w = Writer(open(os.path.join("build.ninja"), "w"))
+    w.comment("Rules")
+    w.newline()
+    out_s = f"$out{os.path.sep}diffbrowsers"
 
-        if imgs:
-            cmd = f"diffbrowsers proof $fonts --imgs -o {out_s}"
-        else:
-            cmd = f"diffbrowsers proof $fonts -o {out_s}"
-        w.rule("proofing", cmd)
-        w.newline()
+    if imgs:
+        cmd = f"diffbrowsers proof $fonts --imgs -o {out_s}"
+    else:
+        cmd = f"diffbrowsers proof $fonts -o {out_s}"
+    w.rule("proofing", cmd)
+    w.newline()
 
-        # Setup build
-        w.comment("Build rules")
-        w.build(
-            out,
-            "proofing",
-            variables=dict(
-                fonts=[f.reader.file.name for f in fonts],
-                out=out,
-            ),
-        )
-        w.close()
-        cwd = os.getcwd()
-        os.chdir(dir_)
-        ninja._program("ninja", [])
-        os.chdir(cwd)
+    # Setup build
+    w.comment("Build rules")
+    w.build(
+        out,
+        "proofing",
+        variables=dict(
+            fonts=[f.reader.file.name for f in fonts],
+            out=out,
+        ),
+    )
+    w.close()
+    ninja._program("ninja", [])
 
 
 def run_diffing_tools(
@@ -70,62 +66,58 @@ def run_diffing_tools(
     if not os.path.exists(out):
         os.mkdir(out)
 
-    with tempfile.TemporaryDirectory() as dir_:
-        w = Writer(open(os.path.join(dir_, "build.ninja"), "w"))
-        # Setup rules
-        w.comment("Rules")
-        w.newline()
-        w.comment("Build Hinting docs")
-        if imgs:
-            cmd = f"diffbrowsers diff -fb $fonts_before -fa $fonts_after --imgs -o $out"
-        else:
-            cmd = f"diffbrowsers diff -fb $fonts_before -fa $fonts_after -o $out"
-        w.rule(
+    w = Writer(open(os.path.join("build.ninja"), "w"))
+    # Setup rules
+    w.comment("Rules")
+    w.newline()
+    w.comment("Build Hinting docs")
+    if imgs:
+        cmd = f"diffbrowsers diff -fb $fonts_before -fa $fonts_after --imgs -o $out"
+    else:
+        cmd = f"diffbrowsers diff -fb $fonts_before -fa $fonts_after -o $out"
+    w.rule(
+        "diffbrowsers",
+        cmd,
+    )
+    w.newline()
+
+    w.comment("Run diffenator")
+    w.rule("diffenator", "diffenator $font_before $font_after -c $coords -o $out")
+    w.newline()
+
+    # Setup build
+    w.comment("Build rules")
+    if diffbrowsers:
+        diffbrowsers_out = os.path.join(out, "diffbrowsers")
+        w.build(
+            diffbrowsers_out,
             "diffbrowsers",
-            cmd,
+            variables=dict(
+                fonts_before=[os.path.abspath(f.reader.file.name) for f in fonts_before],
+                fonts_after=[os.path.abspath(f.reader.file.name) for f in fonts_after],
+                out=diffbrowsers_out,
+            ),
         )
-        w.newline()
-
-        w.comment("Run diffenator")
-        w.rule("diffenator", "diffenator $font_before $font_after -c $coords -o $out")
-        w.newline()
-
-        # Setup build
-        w.comment("Build rules")
-        if diffbrowsers:
-            diffbrowsers_out = os.path.join(out, "diffbrowsers")
+    if diffenator:
+        for style, font_before, font_after, coords in matcher(
+            fonts_before, fonts_after
+        ):
+            style = style.replace(" ", "-")
             w.build(
-                diffbrowsers_out,
-                "diffbrowsers",
+                os.path.join(out, style),
+                "diffenator",
                 variables=dict(
-                    fonts_before=[os.path.abspath(f.reader.file.name) for f in fonts_before],
-                    fonts_after=[os.path.abspath(f.reader.file.name) for f in fonts_after],
-                    out=diffbrowsers_out,
+                    font_before=font_before,
+                    font_after=font_after,
+                    coords=dict_coords_to_string(coords),
+                    out=style,
                 ),
             )
-        if diffenator:
-            for style, font_before, font_after, coords in matcher(
-                fonts_before, fonts_after
-            ):
-                style = style.replace(" ", "-")
-                w.build(
-                    os.path.join(out, style),
-                    "diffenator",
-                    variables=dict(
-                        font_before=font_before,
-                        font_after=font_after,
-                        coords=dict_coords_to_string(coords),
-                        out=style,
-                    ),
-                )
-        w.close()
-        cwd = os.getcwd()
-        os.chdir(dir_)
-        ninja._program("ninja", [])
-        os.chdir(cwd)
+    w.close()
+    ninja._program("ninja", [])
 
 
-def _fullname(ttfont):
+def _static_fullname(ttfont):
     return (
         f"{ttfont['name'].getBestFamilyName()} {ttfont['name'].getBestSubFamilyName()}"
     )
@@ -152,7 +144,7 @@ def matcher(fonts_before, fonts_after):
             for n, coords in vf_names:
                 before[n] = (os.path.abspath(font.reader.file.name), coords)
         else:
-            before[_fullname(font)] = (os.path.abspath(font.reader.file.name), {})
+            before[_static_fullname(font)] = (os.path.abspath(font.reader.file.name), {})
 
     for font in fonts_after:
         if "fvar" in font:
@@ -160,7 +152,7 @@ def matcher(fonts_before, fonts_after):
             for n, coords in vf_names:
                 after[n] = (os.path.abspath(font.reader.file.name), coords)
         else:
-            after[_fullname(font)] = (os.path.abspath(font.reader.file.name), {})
+            after[_static_fullname(font)] = (os.path.abspath(font.reader.file.name), {})
 
     shared = set(before.keys()) & set(after.keys())
     res = []
