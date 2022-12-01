f2f21e0921e21eccf0735f6e12528f01c6bbe370
Marc Foley <m.foley.88@gmail.com>
2022-12-01 11:26:43 +0000

    allow main func to accept args instead of using argparse

--- src/diffenator/__main__.py
+++ src/diffenator/__main__.py
@@ -1,40 +1,42 @@
 #!/usr/bin/env python3
 from __future__ import annotations
-import argparse
+from argparse import ArgumentParser, Namespace
 from fontTools.ttLib import TTFont
 from diffenator import ninja_diff, ninja_proof
 import ninja
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(
-        dest="command", required=True, metavar='"proof" or "diff"'
-    )
-    universal_options_parser = argparse.ArgumentParser(add_help=False)
-    universal_options_parser.add_argument(
-        "--out", "-o", help="Output dir", default="out"
-    )
-    universal_options_parser.add_argument(
-        "--imgs", help="Generate images", action="store_true", default=False
-    )
-    universal_options_parser.add_argument("--filter-styles", default=None)
-    proof_parser = subparsers.add_parser(
-        "proof",
-        parents=[universal_options_parser],
-        help="Generate html proofing documents for a family",
-    )
-    proof_parser.add_argument("fonts", nargs="+")
+def main(**kwargs):
+    if kwargs:
+        args = Namespace(**kwargs)
+    else:
+        parser = ArgumentParser()
+        subparsers = parser.add_subparsers(
+            dest="command", required=True, metavar='"proof" or "diff"'
+        )
+        universal_options_parser = ArgumentParser(add_help=False)
+        universal_options_parser.add_argument(
+            "--out", "-o", help="Output dir", default="out"
+        )
+        universal_options_parser.add_argument(
+            "--imgs", help="Generate images", action="store_true", default=False
+        )
+        universal_options_parser.add_argument("--filter-styles", default=None)
+        proof_parser = subparsers.add_parser(
+            "proof",
+            parents=[universal_options_parser],
+            help="Generate html proofing documents for a family",
+        )
+        proof_parser.add_argument("fonts", nargs="+")
 
-    diff_parser = subparsers.add_parser(
-        "diff",
-        parents=[universal_options_parser],
-    )
-    diff_parser.add_argument("--fonts-before", "-fb", nargs="+", required=True)
-    diff_parser.add_argument("--fonts-after", "-fa", nargs="+", required=True)
-    diff_parser.add_argument("--user-wordlist", default=None)
-
-    args = parser.parse_args()
+        diff_parser = subparsers.add_parser(
+            "diff",
+            parents=[universal_options_parser],
+        )
+        diff_parser.add_argument("--fonts-before", "-fb", nargs="+", required=True)
+        diff_parser.add_argument("--fonts-after", "-fa", nargs="+", required=True)
+        diff_parser.add_argument("--user-wordlist", default=None)
+        args = parser.parse_args()
 
     if args.command == "proof":
         fonts = [TTFont(f) for f in args.fonts]
