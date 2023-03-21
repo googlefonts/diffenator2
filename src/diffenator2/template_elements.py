0f5eebf5e9b18b2ea17be0b00c9738be5e247f99
Marc Foley <m.foley.88@gmail.com>
2023-03-21 09:44:25 +0000

    match fonts by styles only. Skip family name

--- src/diffenator2/template_elements.py
+++ src/diffenator2/template_elements.py
@@ -82,11 +82,11 @@ class CSSFontStyle(Renderable):
         if self.suffix:
             self.cssfamilyname = f"{self.suffix} {self.familyname}"
             self.class_name = (
-                f"{self.suffix} {self.familyname} {self.stylename}".replace(" ", "-")
+                f"{self.suffix} {self.stylename}".replace(" ", "-")
             )
         else:
             self.cssfamilyname = self.familyname
-            self.class_name = f"{self.familyname} {self.stylename}".replace(" ", "-")
+            self.class_name = f"{self.stylename}".replace(" ", "-")
 
 
 @dataclass
