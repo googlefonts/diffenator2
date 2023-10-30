b5b7ade276a1e60049c467a917e46f01335508c0
Marc Foley <m.foley.88@gmail.com>
2023-10-30 13:59:15 +0000

    Ninja refactor

--- src/diffenator2/__main__.py
+++ src/diffenator2/__main__.py
@@ -6,7 +6,7 @@ from diffenator2 import ninja_diff, ninja_proof, THRESHOLD, NINJA_BUILD_FILE
 from diffenator2.font import DFont
 from diffenator2.html import build_index_page
 from diffenator2.renderer import FONT_SIZE
-
+from pkg_resources import resource_filename
 
 
 def main(**kwargs):
@@ -49,45 +49,25 @@ def main(**kwargs):
         diff_parser.add_argument("--no-diffenator", default=False, action="store_true")
         diff_parser.add_argument("--threshold", "-t", type=float, default=THRESHOLD)
         diff_parser.add_argument("--precision", default=FONT_SIZE)
+        # TODO this can just be precision
+        diff_parser.add_argument("--font-size", default=FONT_SIZE)
         diff_parser.add_argument("--no-tables", action="store_true", help="Skip diffing font tables")
         diff_parser.add_argument("--no-words", action="store_true", help="Skip diffing wordlists")
+        parser.add_argument(
+        "--diffenator-template",
+        default=resource_filename(
+            "diffenator2", os.path.join("templates", "diffenator.html")
+        ),
+    )
         args = parser.parse_args()
 
-    if os.path.exists(NINJA_BUILD_FILE):
-        os.remove(NINJA_BUILD_FILE)
-    if os.path.exists(".ninja_log"):
-        os.remove(".ninja_log")
     if args.command == "proof":
-        fonts = [DFont(f) for f in args.fonts]
-        ninja_proof(
-            fonts,
-            out=args.out,
-            imgs=args.imgs,
-            styles=args.styles,
-            filter_styles=args.filter_styles,
-            characters=args.characters,
-            pt_size=args.pt_size,
-            user_wordlist=args.user_wordlist,
-        )
+        args.fonts = [DFont(f) for f in args.fonts]
+        ninja_proof(**vars(args))
     elif args.command == "diff":
-        fonts_before = [DFont(f) for f in args.fonts_before]
-        fonts_after = [DFont(f) for f in args.fonts_after]
-        ninja_diff(
-            fonts_before,
-            fonts_after,
-            out=args.out,
-            imgs=args.imgs,
-            styles=args.styles,
-            characters=args.characters,
-            diffenator=False if args.no_diffenator else True,
-            user_wordlist=args.user_wordlist,
-            filter_styles=args.filter_styles,
-            pt_size=args.pt_size,
-            threshold=args.threshold,
-            precision=args.precision,
-            no_words=args.no_words,
-            no_tables=args.no_tables,
-        )
+        args.fonts_before = [DFont(f) for f in args.fonts_before]
+        args.fonts_after = [DFont(f) for f in args.fonts_after]
+        ninja_diff(**vars(args))
     else:
         raise NotImplementedError(f"{args.command} not supported")
     # TODO (Marc F) find a better home for this when refactoring
